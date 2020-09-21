7daf30e5b5d2a17d0553986ea086d3d889494a5c
manish <manish.singh35200@gmail.com>
2020-09-21 17:01:23 +0530

    added a new file coordinat_extraction py, this file read coordinates from image, it also crops image our of complete frame for further processing"

--- vid_to_frame.py
+++ vid_to_frame.py
@@ -1,11 +1,19 @@
 import cv2
 import os
-vidcap = cv2.VideoCapture(r"road_survey_vid\gps_cam1.mp4")
+vidcap = cv2.VideoCapture(r"road_survey_vid\gps-cam.mp4")
 success,image = vidcap.read()
 count = 0
 path = r"C:\Users\manis\PycharmProjects\pothole_heatmap\frames"
+# code below will extract one frame after every 0.1 sec, but will change the frequency of picking frame laatter when tuning
+seconds = 100
+fps = vidcap.get(cv2.CAP_PROP_FPS) # Gets the frames per second
+print(fps)
+multiplier = int(fps * seconds)
+frame_no = 0
 while success:
+  frame_no += 1
   cv2.imwrite(os.path.join(path, "frame%d.jpg" % count), image)     # save frame as JPEG file
   success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 100
+  count += 1 # this stes the frequency of capturing frames
+  print('Read a new frame at ', count, " :", success)
+
